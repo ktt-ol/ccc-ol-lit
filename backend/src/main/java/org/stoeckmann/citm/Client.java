61eaa729a65b82e1e62af925a50a7e6f398d0722
Tobias Stoeckmann <tobias@stoeckmann.org>
2014-10-08 20:07:44 +0200

    comments are not supposed to lie

--- backend/src/main/java/org/stoeckmann/citm/Client.java
+++ backend/src/main/java/org/stoeckmann/citm/Client.java
@@ -36,9 +36,9 @@ import org.stoeckmann.citm.domain.format.Spec;
 public class Client {
     /**
      * Method handling HTTP GET requests. The returned object will be sent to
-     * the client as "text/plain" media type.
+     * the client as "application/json" media type.
      *
-     * @return String that will be returned as a text/plain response.
+     * @return Spec object that will be returned as a application/json response.
      * @throws IOException
      */
     @GET
